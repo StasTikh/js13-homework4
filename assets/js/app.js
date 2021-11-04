6ab9bdf9bebe9ca4b2246ac5e0f383a8d32a9f35
StasTikh <tmihatelefan@gmail.com>
2021-11-04 15:08:51 +0200

    shorter merge + output

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,63 @@
 let today = new Date().toLocaleString();
 todayHeader.innerHTML += today;
 
-function countryCreator(countryC, nameC, flagC){
+// function countryCreator(countryC, nameC, flagC){
     
-    this.countryC = countryC;
-    this.nameC = nameC;
-    this.flagC = flagC;  
-}
+//     this.countryC = countryC;
+//     this.nameC = nameC;
+//     this.flagC = flagC;  
+// }
 
-function currencyCreator(currencyCode, nameCc, rateCc) {
-    this.currencyCode = currencyCode;
-    this.nameCc = nameCc;
-    this.rateCc = rateCc;
-}
+// function currencyCreator(currencyCode, nameCc, rateCc) {
+//     this.currencyCode = currencyCode;
+//     this.nameCc = nameCc;
+//     this.rateCc = rateCc;
+// }
 
-function finalCreate(finalName, finalCc, finalRate, finalFlag, countryName) {
-    this.finalName = finalName;    
-    this.finalCc = finalCc;
-    this.finalRate = finalRate;
-    this.finalFlag = finalFlag;
-    this.countryName = countryName;
-}
+// function finalCreate(finalName, finalCc, finalRate, finalFlag, countryName) {
+//     this.finalName = finalName;    
+//     this.finalCc = finalCc;
+//     this.finalRate = finalRate;
+//     this.finalFlag = finalFlag;
+//     this.countryName = countryName;
+// }
+
+// const rateURL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+// const countryURL = "https://restcountries.com/v3.1/all";
+
+// let ratesData = await fetch(rateURL);
+// ratesData = await ratesData.json();
+
+// let countriesData = await fetch(countryURL);
+// countriesData = await countriesData.json();
+
+// let outputObj = [{}];
+
+// countriesData = countriesData
+//                     .filter(item => item.currencies)
+//                     .map(item => ({
+//                         title: item.name.common,
+//                         flag: item.flags.png,
+//                         cc: Object.keys(item.currencies)
+//                     }));
+
+// for (let rate of ratesData) {
+//     rate.countriesData = countriesData.filter(item => item.cc.includes(ratesData.cc));
+// }
+
+// console.log(countriesData);
+
+// for (let i = 0; i <= ratesData.length; i++) {
+//     outputObj.name = ratesData[i].txt;
+//     outputObj.rates = ratesData[i].cc;
+//     for (let j = 0; j <= countriesData.length; j++) {
+//         if (countriesData[i]?.cc == ratesData[j]?.cc) {
+//            outputObj.flags.push(countriesData?.flag);
+//         }    
+//     }
+// }
+
+// console.log(outputObj);
 
 const rateURL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
 const countryURL = "https://restcountries.com/v3.1/all";
@@ -31,84 +68,78 @@ ratesData = await ratesData.json();
 let countriesData = await fetch(countryURL);
 countriesData = await countriesData.json();
 
+let outputArr = [];
 
 
-let countriesCc = [], 
-    countriesName = [], 
-    countriesFlags = [], 
-    currencyCc = [], 
-    currencyName = [],
-    currencyRate = [],
-    finalName = [],
-    finalCode = [],
-    finalRates = [],
-    finalFlags = [],
-    countryNames = [];
+countriesData = countriesData
+                    .filter(item => item.currencies)
+                    .map(item => ({
+                        title: item.name.common,
+                        flag: item.flags.png,
+                        cc: Object.keys(item.currencies)
+                    }));
 
-let newCountry = [], newCurrency = [], finishCode = [];
+for (let rate of ratesData) {
+    rate.countriesData = countriesData.filter(item => item.cc.includes(ratesData.cc));
+}
 
+console.log(countriesData);
 
+//country is not object
 
-for (let item in countriesData){
-    if(countriesData[item].currencies !== undefined && countriesData[item].flags !== undefined && countriesData[item].name.official !== undefined ) {
-        countriesCc.push(Object.keys(countriesData[item].currencies)[0]);
-        countriesName.push(countriesData[item].name.official);
-        countriesFlags.push(countriesData[item].flags.png)
+for (let i = 0; i < ratesData.length; i++) {
+    let outputObj = new Object(); 
+    outputObj.txt = ratesData[i]?.txt;
+    outputObj.rates = ratesData[i]?.rate;
+    outputObj.countries = [];
+    outputObj.flags = [];
+    for (let j = 0; j < countriesData.length; j++) {
+        if (countriesData[j]?.cc == ratesData[i]?.cc) {
+           outputObj.countries.push(countriesData[j]?.title);
+           outputObj.flags.push(countriesData[j]?.flag);
+        }    
     }
+  outputArr.push(outputObj);
 }
 
-for (let item in ratesData){
-    currencyCc.push(ratesData[item].cc);
-    currencyName.push(ratesData[item].txt);
-    currencyRate.push(ratesData[item].rate);
+// country is object includes flags and names
+
+// for (let i = 0; i < ratesData.length; i++) {
+//     let outputObj = new Object(); 
+//     outputObj.txt = ratesData[i]?.txt;
+//     outputObj.rates = ratesData[i]?.rate;
+//     outputObj.countries = [];
+//     for (let j = 0; j < countriesData.length; j++) {
+//         if (countriesData[j]?.cc == ratesData[i]?.cc) {
+//            let outputCountry = new Object();
+//            outputCountry.name = countriesData[j]?.title;
+//            outputCountry.flag = countriesData[j]?.flag;
+//            outputObj.countries.push(outputCountry);
+//         }    
+//     }
+//   outputArr.push(outputObj);
+// }
+
+for (let i = 1; i <= 4; i++){
+    outputArr.pop();
 }
 
-for (let i = 0; i <= countriesCc.length; i++) {    
-        let countTry = new countryCreator (countriesCc[i], countriesName[i], countriesFlags[i]);
-        if(countriesCc[i] !== undefined && countriesName[i] !== undefined && countriesFlags[i] !== undefined) {
-            newCountry.push(countTry);  
-        }
-}
+console.log(outputArr);
 
-for (let i = 0; i <= ratesData.length; i++) {    
-    let currencTry = new currencyCreator (currencyCc[i], currencyName[i], currencyRate[i]);
-    if(currencyCc[i] !== undefined && currencyName[i] !== undefined && currencyRate[i] !== undefined) {
-        newCurrency.push(currencTry); 
-    }
-          
-}
-
-console.log(newCountry, newCurrency);
-
-
-for (let i = 0; i <= newCountry.length; i++) {
-    for (let j = 0; j <= newCurrency.length; j++) {
-        if(newCountry[i]?.countryC == newCurrency[j]?.currencyCode) {
-            finalName.push(newCurrency[j]?.nameCc);
-            finalCode.push(newCurrency[j]?.currencyCode);
-            finalRates.push(newCurrency[j]?.rateCc);
-            finalFlags.push(newCountry[i]?.flagC);
-            countryNames.push(newCountry[i]?.nameC);
-        }
-    }
-}
-for (let i = 0; i <= finalName.length; i++) {
-    let finalTry = new finalCreate(finalName[i], finalCode[i], finalRates[i], finalFlags[i], countryNames[i]);
-    if(finalName[i] !== undefined && finalCode[i] !== undefined && finalRates[i] !== undefined && finalFlags[i] !== undefined && countryNames[i] !== undefined) {
-        finishCode.push(finalTry); 
-    }
-}
-
-infoPlace.innerHTML = finishCode.map (item => `
-                <div class="d-flex align-items-center border border-3 m-3 p-1 w-50 mx-auto">    
-                    <h5>${item.finalName}</h5>
-                    
-                    <div class="flex-grow-1 ms-5 w-25">
-                        <h5>${item.finalRate}</h5>
-                    </div>
-                        <img class="img-thumbnail m-1  border border-2" src="${item.finalFlag}" title="${item.countryName}"" style= "width: 100px; height:100px">
+infoPlace.innerHTML = outputArr.map (item => `
+                <div id="countryCard" class="d-flex align-items-center border border-3 m-3 p-1 w-50 mx-auto">    
+                <h5 class="m-1">${item.txt}: </h5>
+                <h5 class="m-1">${item.rates}</h5>
+                <img src="${item.flags}" title="${item.countries}" style="width: 75px; height: 75px" class="m-2 border border-1"</img>       
                 </div>`
 ).join("");
     
-    
-
+for (let i = 0; i < outputArr.length; i++){
+    if(outputArr[i]?.flags.length != 1) {
+       for(let j = 0; j <= outputArr[i].flags.length; j++){
+            countryCard.insertAdjacentHTML('beforeend', 
+                                           `<img src="${outputArr[i].flags[j]}" title="${outputArr[i].countries[j]}" style="width: 75px; height: 75px" class="m-2 border border-1"</img> 
+            `)
+       }  
+    }
+}
